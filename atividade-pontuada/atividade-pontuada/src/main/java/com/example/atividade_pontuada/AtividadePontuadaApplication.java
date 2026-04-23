1eb679f42d9d6b4459cda9a0892be77e65caed7a
luizgustavo071 <luiz.g.cruz@ba.estudante.senai.br>
2026-04-22 21:33:56 -0300

    Add files via upload

---
+++ atividade-pontuada/atividade-pontuada/src/main/java/com/example/atividade_pontuada/AtividadePontuadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividade_pontuada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadePontuadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadePontuadaApplication.class, args);
+	}
+
+}
